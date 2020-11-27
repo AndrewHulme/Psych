1c51e4206ff7896e16c50f8605fa364270284002
Andrew Hulme <andrewhulme1993@gmail.com>
2020-11-27 13:53:35 +0000

    Deletes several unnecessary files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+
+// REDUCER
+
+// DISPATCH - EXECUTES ACTION
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
