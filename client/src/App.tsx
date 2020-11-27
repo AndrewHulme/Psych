748780e74566d6cf42dbe8fd2173c277bd94530e
Andrew Hulme <andrewhulme1993@gmail.com>
2020-11-27 22:25:18 +0000

    Adds basic material ui components to front page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Button, Typography, Container } from "@material-ui/core";
 // import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h3" gutterBottom>
+        Psych?!
+      </Typography>
+
+      <Button color="primary" variant="outlined">
+        Play
+      </Button>
+    </Container>
   );
 }
 
