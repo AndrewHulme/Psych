cd52f1efea96ecfa63577c0b9a288ac22a487b44
Andrew Hulme <andrewhulme1993@gmail.com>
2020-12-22 22:27:34 +0000

    Conditionally renders the userNameInput component if user name provided

--- client/src/components/homepage.tsx
+++ client/src/components/homepage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Grid, Container } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-// import UserNameInput from "./userNameInput";
+import UserNameInput from "./userNameInput";
 
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
@@ -29,46 +29,52 @@ function Homepage() {
         console.log(data.currentUser.name);
 
         return (
-          <div className="homepage">
-            <Container component="main" maxWidth="xs">
-              <Grid container spacing={2}>
-                <Grid item>
-                  <Link to={"/join"}>
-                    <Button
-                      style={{
-                        maxWidth: "400px",
-                        maxHeight: "100px",
-                        minWidth: "400px",
-                        minHeight: "100px",
-                        fontSize: "35px",
-                      }}
-                      color="primary"
-                      variant="outlined"
-                    >
-                      JOIN A GAME
-                    </Button>
-                  </Link>
-                </Grid>
+          <div>
+            {data.currentUser.name === "Player" ? (
+              <UserNameInput />
+            ) : (
+              <div className="homepage">
+                <Container component="main" maxWidth="xs">
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <Link to={"/join"}>
+                        <Button
+                          style={{
+                            maxWidth: "400px",
+                            maxHeight: "100px",
+                            minWidth: "400px",
+                            minHeight: "100px",
+                            fontSize: "35px",
+                          }}
+                          color="primary"
+                          variant="outlined"
+                        >
+                          JOIN A GAME
+                        </Button>
+                      </Link>
+                    </Grid>
 
-                <Grid item>
-                  <Link to={"/start"}>
-                    <Button
-                      style={{
-                        maxWidth: "400px",
-                        maxHeight: "100px",
-                        minWidth: "400px",
-                        minHeight: "100px",
-                        fontSize: "35px",
-                      }}
-                      color="primary"
-                      variant="outlined"
-                    >
-                      START A GAME
-                    </Button>
-                  </Link>
-                </Grid>
-              </Grid>
-            </Container>
+                    <Grid item>
+                      <Link to={"/start"}>
+                        <Button
+                          style={{
+                            maxWidth: "400px",
+                            maxHeight: "100px",
+                            minWidth: "400px",
+                            minHeight: "100px",
+                            fontSize: "35px",
+                          }}
+                          color="primary"
+                          variant="outlined"
+                        >
+                          START A GAME
+                        </Button>
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Container>
+              </div>
+            )}
           </div>
         );
       }}
