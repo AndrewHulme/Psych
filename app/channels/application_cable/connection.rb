8b1496b554316a4493cc5133fe0c48fbc7d25bf6
Ethaning <ethanjcarter@hotmail.co.uk>
2020-12-25 21:20:34 +0000

    Add setup for RoomChannel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = User.find_or_create_by(visitor_key: cookies[:visitor_key])
+    end
   end
 end
