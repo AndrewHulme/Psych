7a0538410ee075c30b8ff4c52e3f710ed7a802f4
Ethaning <ethanjcarter@hotmail.co.uk>
2021-01-06 00:43:34 +0000

    Fix cors config resource

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV["ALLOWED_HOSTS"] ? ENV["ALLOWED_HOSTS"].split(",").map(&:strip) : ""
 
-    resource "/zendesk/sso", headers: :any, methods: :post, expose: %w[Authorization]
+    resource "/", headers: :any, methods: :post, expose: %w[Authorization]
     resource "/graphql", headers: %w[Authorization], methods: :any, expose: %w[Authorization], max_age: 3600,
              credentials: true
   end
