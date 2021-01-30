52c89fc34abe762b5cbdc267c079dad937c524c9
Ethaning <ethanjcarter@hotmail.co.uk>
2021-01-30 17:53:43 +0000

    Install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,15 @@ class User < ApplicationRecord
   has_many :answers
   has_many :votes
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :visitor_key, uniqueness: { case_sensitive: false }, allow_nil: true
   validates :name, length: { minimum: MIN_USERNAME_LENGTH, maximum: MAX_USERNAME_LENGTH }
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable, :async,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   delegate :current_round, to: :room
 
