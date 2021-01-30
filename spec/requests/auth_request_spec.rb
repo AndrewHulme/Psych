8aa14409ae49f9992854960c6f96bad96b51be64
Ethaning <ethanjcarter@hotmail.co.uk>
2021-01-30 20:00:33 +0000

    Depreciate auth_request_spec.rb

--- spec/requests/auth_request_spec.rb
+++ spec/requests/auth_request_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe AuthController, type: :request do
-  describe "POST /visitor-key" do
-    subject { post "/visitor-key" }
+# RSpec.describe AuthController, type: :request do
+#   describe "POST /visitor-key" do
+#     subject { post "/visitor-key" }
 
-    it "returns http success" do
-      subject
+#     it "returns http success" do
+#       subject
 
-      expect(response).to have_http_status(:success)
-    end
+#       expect(response).to have_http_status(:success)
+#     end
 
-    it "sets cookies.visitor_key and ensures the value persists on subsequent requests" do
-      subject
+#     it "sets cookies.visitor_key and ensures the value persists on subsequent requests" do
+#       subject
 
-      initial_visitor_key = cookies[:visitor_key]
-      expect(cookies[:visitor_key].present?).to eq(true)
+#       initial_visitor_key = cookies[:visitor_key]
+#       expect(cookies[:visitor_key].present?).to eq(true)
 
-      post "/visitor-key"
+#       post "/visitor-key"
 
-      expect(cookies[:visitor_key].present?).to eq(true)
-      expect(cookies[:visitor_key]).to eq(initial_visitor_key)
-    end
-  end
-end
+#       expect(cookies[:visitor_key].present?).to eq(true)
+#       expect(cookies[:visitor_key]).to eq(initial_visitor_key)
+#     end
+#   end
+# end
